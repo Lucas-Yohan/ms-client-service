0e08352ff2f059f70d30d21b2475defa38c950ab
Lucas Yohan <lucalen.y@live.com>
2025-10-04 17:33:36 -0300

    Implementado OpenFeign

--- hr-payroll/src/main/java/com/ms_payroll/hr_payroll/services/PaymentService.java
+++ hr-payroll/src/main/java/com/ms_payroll/hr_payroll/services/PaymentService.java
@@ -2,28 +2,18 @@ package com.ms_payroll.hr_payroll.services;
 
 import com.ms_payroll.hr_payroll.entities.Payment;
 import com.ms_payroll.hr_payroll.entities.Worker;
+import com.ms_payroll.hr_payroll.feignclients.WorkerFeignClient;
 import lombok.RequiredArgsConstructor;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
-import org.springframework.web.client.RestTemplate;
-
-import java.util.HashMap;
-import java.util.Map;
 
 @Service
 @RequiredArgsConstructor
 public class PaymentService {
 
-    @Value("${hr-worker.host}")
-    private String workerHost;
-
-    private final RestTemplate restTemplate;
+    private final WorkerFeignClient workerFeignClient;
 
     public Payment getPayment(Long id, int days) {
-        Map<String, String> uriVariables = new HashMap<>();
-        uriVariables.put("id", "" + id);
-
-        Worker worker = restTemplate.getForObject(workerHost + "/workers/{id}", Worker.class, uriVariables);
+        Worker worker = workerFeignClient.getById(id).getBody();
         return new Payment(worker.getName(), days, worker.getDailyIncome());
     }
 
