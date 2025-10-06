c96a3414b7d86ac75f2912fc7109eb8e4be567de
Lucas Yohan <lucalen.y@live.com>
2025-10-05 21:49:21 -0300

    Configurado ports payroll e worker para Eureka Server

--- hr-payroll/src/main/java/com/ms_payroll/hr_payroll/HrPayrollApplication.java
+++ hr-payroll/src/main/java/com/ms_payroll/hr_payroll/HrPayrollApplication.java
@@ -7,7 +7,6 @@ import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @EnableFeignClients
 @SpringBootApplication
-@LoadBalancerClient(name = "hr-worker")
 public class HrPayrollApplication {
 
 	public static void main(String[] args) {
--- hr-worker/src/main/java/com/hr_worker/hr_worker/HrWorkerApplication.java
+++ hr-worker/src/main/java/com/hr_worker/hr_worker/HrWorkerApplication.java
@@ -3,6 +3,7 @@ package com.hr_worker.hr_worker;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+
 @SpringBootApplication
 public class HrWorkerApplication {
 
